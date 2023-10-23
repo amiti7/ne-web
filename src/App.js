74f066f9733fa04c5279fa1bf34fe24f53486e61
Amit Verma <cs.amit.v@gmail.com>
2023-10-23 17:57:50 +0530

    begin GA hitting

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useEffect} from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 // import enquire from './pages/enquire';
@@ -9,6 +10,10 @@ import ReactGA4 from "react-ga4";
 ReactGA4.initialize('G-9T21YZCGK0');
 
 function App() {
+  useEffect(()=>{
+    console.log('firing app load', window.location.pathname)
+    ReactGA4.send({ hitType: "pageview", page: window.location.pathname });
+    },[])
   return (
     <Router >
       <Routes>
